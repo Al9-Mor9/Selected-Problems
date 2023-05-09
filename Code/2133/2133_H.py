cc780915366ecdec9b171b7752242c4cffd5cf9f
wocjs <wocjs602@gmail.com>
2023-05-09 14:37:54 +0900

    1437

--- Code/2133/2133_H.py
+++ Code/2133/2133_H.py
@@ -8,10 +8,12 @@ if n % 2:
     exit(0)
 arr = [0] * 31
 arr[2] = 3
+
 '''
 홀수일 때는 0
 짝수일 때는 [n-2] * 3 + 2 * { n-4까지 합 } + 2
 '''
+
 for i in range(4, 31, 2):
     arr[i] += arr[i-2] * 3
     for j in range(0, i-2, 2):
