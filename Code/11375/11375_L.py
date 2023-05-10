84d91e1f00ed6a0d64e6f2476512ee1a7db31131
임수형 <suhyeng119@gmail.com>
2023-05-11 08:51:28 +0900

    Update 11375_L.py

--- Code/11375/11375_L.py
+++ Code/11375/11375_L.py
@@ -2,31 +2,6 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-# sys.setrecursionlimit(100000)
-# n, m = map(int, input().split())
-# adj = [list(map(int,input().split()))[1:] for _ in range(n)]
-# match= [False] * (m + 1)
-# result=0
-
-# def dfs(cur):
-#     for job in adj[cur]:      # cur 에서 할 수 있는 job
-#        if not is_done[job]:    # 만약 job이 아직 안되었다면, 
-#           is_done[job] = True   # 처리하고, 담당이없거나, 담당을 밀어낼 수 있다면 내꺼.
-#           if not match[job] or dfs(match[job]):
-#             match[job] = cur
-#             return True    
-#     # cur에서 할 수 있는 job이 없고,
-#     # job이 있는데 다 되어 있고,
-#     # job을 이미 했거나 그 일을 한 놈이 다른 일을 찾지 못한다면,
-#     return False
-
-# for i in range(n):
-#    is_done = [False] * (m + 1) # 밀어내야 하기 때문에 일단 일은 안되어있다고 하자.
-#    if dfs(i): result += 1
-
-# print(result)
-
-
 
 n, m = map(int, input().split())
 adj = [0] + [list(map(int,input().split()))[1:] for _ in range(n)]
